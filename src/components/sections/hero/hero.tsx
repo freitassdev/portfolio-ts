4bd7dd2ec1faac321e8b5117b2d86730e78aaeb9
zuifiti <chelmizinho@gmail.com>
2024-05-01 14:11:37 -0300

    fix: hero section in mobile

--- src/components/sections/hero/hero.tsx
+++ src/components/sections/hero/hero.tsx
@@ -5,10 +5,10 @@ import { useTranslation } from 'react-i18next';
 const HeroSection: React.FC = () => {
     const { t } = useTranslation();
     return (
-        <div className="flex flex-col items-center justify-center gap-8 h-[80vh] text-center p-4">
+        <div className="flex flex-col items-center justify-center max-sm:gap-4 gap-8 h-[80vh] text-center p-4">
             <p className="text-xl font-light font-sans max-lg:text-lg">👋🏼 {t('sections.hero.hello')} <span className='font-medium'>Michel Freitas</span>!</p>
-            <h1 className="text-6xl font-bold font-poppins max-lg:text-5xl max-sm:text-3xl">{t('sections.hero.title')}</h1>
-            <p className="text-xl font-light font-sans max-w-[700px] max-lg:text-lg">{t('sections.hero.description')}</p>
+            <h1 className="text-6xl font-bold font-poppins max-lg:text-5xl max-sm:text-4xl">{t('sections.hero.title')}</h1>
+            <p className="text-xl font-light font-sans max-w-[700px] max-lg:text-lg max-sm:text-base">{t('sections.hero.description')}</p>
 
             <div className="flex gap-5">
                 <Button className='text-foreground'>{t('sections.hero.buttons.contact')}</Button>
